5f83a7af97410205d46ce15ce32843b2fa16dc0f
derekalarson <derekalarson@gmail.com>
2024-07-30 12:46:05 -0600

    fixed dictionary error in test_db

--- tests/test_db.py
+++ tests/test_db.py
@@ -32,55 +32,55 @@ def test_create_user():
         for key in formatted_users[0]:
             assert user[key] == formatted_users[0][key]
 
-formatted_users += [
-    {
-    'username': 'jimmy_abdul',
-    'email': 'jimmy@hi5ive',
-    'password_hash': 'jimmy123',
-    'first_name': 'Jimmy',
-    'last_name': 'Abdul',
-    'bio': 'I wish I was Paula Abdul\'s brother',
-    'profile_picture': generate_avatar_url(),
-    'location': 'New York'
-    },
-    {
-    'username': 'henry_abdul',
-    'email': 'henry@hi5ive',
-    'password_hash': 'henry123',
-    'first_name': 'Henry',
-    'last_name': 'Abdul',
-    'bio': 'I am Paula Abdul\'s brother',
-    'profile_picture': generate_avatar_url(),
-    'location': 'New York'
-    },
-    {
-    'username': 'lenny_kravitz',
-    'email': 'lenny@hi5ive',
-    'password_hash': 'lenny123',
-    'first_name': 'Lenny',
-    'last_name': 'Kravitz',
-    'bio': 'I am a rockstar',
-    'profile_picture': generate_avatar_url(),
-    'location': 'New York'
-    },
-    {
-    'username': 'jimmy_hendrix',
-    'email': 'jimmy@hi5ive',
-    'password_hash': 'jimmy123',
-    'first_name': 'Jimmy',
-    'last_name': 'Hendrix',
-    'bio': 'I am a rockstar',
-    'profile_picture': generate_avatar_url(),
-    'location': 'New York'
-    },
-    {
-    'username': 'hedy_lamar',
-    'email': 'hedy@hi5ive',
-    'password_hash': 'hedy123',
-    'first_name': 'Hedy',
-    'last_name': 'Lamar',
-    'bio': 'I am an actress',
-    'profile_picture': generate_avatar_url(),
-    'location': 'New York'
-    },
-]
+# formatted_users += [
+#     {
+#     'username': 'jimmy_abdul',
+#     'email': 'jimmy@hi5ive',
+#     'password_hash': 'jimmy123',
+#     'first_name': 'Jimmy',
+#     'last_name': 'Abdul',
+#     'bio': 'I wish I was Paula Abdul\'s brother',
+#     'profile_picture': generate_avatar_url(),
+#     'location': 'New York'
+#     },
+#     {
+#     'username': 'henry_abdul',
+#     'email': 'henry@hi5ive',
+#     'password_hash': 'henry123',
+#     'first_name': 'Henry',
+#     'last_name': 'Abdul',
+#     'bio': 'I am Paula Abdul\'s brother',
+#     'profile_picture': generate_avatar_url(),
+#     'location': 'New York'
+#     },
+#     {
+#     'username': 'lenny_kravitz',
+#     'email': 'lenny@hi5ive',
+#     'password_hash': 'lenny123',
+#     'first_name': 'Lenny',
+#     'last_name': 'Kravitz',
+#     'bio': 'I am a rockstar',
+#     'profile_picture': generate_avatar_url(),
+#     'location': 'New York'
+#     },
+#     {
+#     'username': 'jimmy_hendrix',
+#     'email': 'jimmy@hi5ive',
+#     'password_hash': 'jimmy123',
+#     'first_name': 'Jimmy',
+#     'last_name': 'Hendrix',
+#     'bio': 'I am a rockstar',
+#     'profile_picture': generate_avatar_url(),
+#     'location': 'New York'
+#     },
+#     {
+#     'username': 'hedy_lamar',
+#     'email': 'hedy@hi5ive',
+#     'password_hash': 'hedy123',
+#     'first_name': 'Hedy',
+#     'last_name': 'Lamar',
+#     'bio': 'I am an actress',
+#     'profile_picture': generate_avatar_url(),
+#     'location': 'New York'
+#     },
+# ]
