7e9b0356c40a5d76f7919deea99db6970d358ec3
Matthew Martin <matthewmartin117@gmail.com>
2024-08-06 14:42:49 -0400

    changes

--- hi5ive/src/App.js
+++ hi5ive/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route,Navigate } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
-
+import SignUp from './components/SignUp.js';
 import './App.css';
 import { Profile } from './components/Profile';
 import { UserGrid } from './components/UserGrid';
